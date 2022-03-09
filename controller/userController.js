1cd99a74bcb1937fb500ea3403d9f20c037e96ba
Malav <malavnagar90@gmail.com>
2022-03-09 11:18:32 +0530

    changes

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 const UserService = require("../services/userService")
 const User = require('../dto/userdto');
 const bcrypt = require("../utils/encrypt")
+const token = require("../utils/token")
 const userController = {
     register(req,res){
         let hashPassword = bcrypt.doEncrypt(req.body.password)
@@ -10,12 +11,35 @@ const userController = {
         .then(data =>{
             res.status(201).json({
                 message : "Registration Successfully",
-                data:data
             })
         })
         .catch((err)=>{
             res.status(500).json(err.message)
         })
+    },
+    login(req,res){
+        let email  = req.body.email
+        let password = req.body.password
+        const promise = UserService.login(email)
+        promise.then(
+            (data)=>{
+                let pass = bcrypt.compare(password,data.password)
+                if(email === data.email && pass === true ){
+                    const {password,...others} = data._doc
+                    const accessToken = token.createToken({
+                        id:data._id,
+                        isAdmin:data.isAdmin
+                    })
+                    res.status(200).json({...others,accessToken})
+                }
+                else{
+                    res.status(500).json({
+                        message : "Email And Password Not Match....."
+                    })
+                }        
+        }
+         ).catch((err)=>res.status(500).json(err))
+    
     },
     //User Update By Verify Token
     updateUser(req,res){
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,6 +3,7 @@ const Routes = express.Router()
 const userController = require("../controller/userController")
 Routes.put("/:id",userController.updateUser)
 Routes.post("/register",userController.register)
+Routes.post("/login",userController.login)
 Routes.delete("/:id",userController.deleteUser)
 Routes.get("/allUser",userController.fetchAllUser)
 
