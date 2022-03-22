2383bb438c79bb36488873da45026f3ca5bc987e
Malav <malavnagar90@gmail.com>
2022-03-22 16:12:26 +0530

    joi implement to validate user input

--- controller/userController.js
+++ controller/userController.js
@@ -1,88 +1,111 @@
-const UserService = require("../services/userService")
+const UserService = require('../services/userService');
 const User = require('../dto/userdto');
-const bcrypt = require("../utils/encrypt")
-const token = require("../utils/token")
+const bcrypt = require('../utils/encrypt');
+const token = require('../utils/token');
+const joi = require('joi');
 const userController = {
-    register(req,res){
-        let hashPassword = bcrypt.doEncrypt(req.body.password)
-        const user = new User(req.body.name,hashPassword,req.body.phone,req.body.email)
-        const promise = UserService.addUser(user)
-        promise
-        .then(data =>{
-            res.status(201).json({
-                message : "Registration Successfully",
-            })
-        })
-        .catch((err)=>{
-            res.status(500).json(err.message)
-        })
-    },
-    login(req,res){
-        let email  = req.body.email
-        let password = req.body.password
-        const promise = UserService.login(email)
-        promise.then(
-            (data)=>{
-                let pass = bcrypt.compare(password,data.password)
-                if(email === data.email && pass === true ){
-                    const {password,...others} = data._doc
-                    const accessToken = token.createToken({
-                        id:data._id,
-                        isAdmin:data.isAdmin
-                    })
-                    res.status(200).json({...others,accessToken})
-                }
-                else{
-                    res.status(500).json({
-                        message : "Email And Password Not Match....."
-                    })
-                }        
-        }
-         ).catch((err)=>res.status(500).json(err))
-    
-    },
-    //User Update By Verify Token
-    updateUser(req,res){
-        let id  = req.params.id
-        let userData = req.body
-        const promise = UserService.updateUser(id,userData)
-        promise.then((data)=>{
-            console.log(data)
-            res.status(201).json({
-                message : "Update Successfully"
-            })
-        }).catch((err)=>{
-            console.log(err.message)
-        })
-    },
-    // Delete User
-    deleteUser(req,res){
-        let id = req.params.id
-        const promise = UserService.delete(id)
-        promise
-        .then((data)=>{
-            console.log(data)
-            res.status(200).json({
-                message : "Delete Successfully"
-            })
-        })
-        .catch((err)=>{
-            console.log(err.message)
-        })
-    },
-    
-    // fetch All Users
-    fetchAllUser(req,res){
-        const query = req.query.new
-        console.log(query)
-        const promise = UserService.getAllUsers(query);
-        promise.then((data)=>{
-            res.send(data)
-            console.log(data)
-        }).catch((err)=>{
-            console.log(err.message)
-        })
-    }
-}
+  register(req, res) {
+    let name = req.body.name;
+    let hashPassword = bcrypt.doEncrypt(req.body.password);
+    let email = req.body.email;
+    let phone = req.body.email;
 
-module.exports = userController
+    const schema = joi.object({
+      name: joi.string().min(3).max(15).required(),
+      password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+      email: joi.string().email({
+        minDomainSegments: 2,
+        tlds: {allow: ['com', 'net']},
+      }),
+      phone: joi.number().less(10).required(),
+    });
+    let result = schema.validate(req.body);
+    if (result.error) {
+      res.status(400).send(result.error.details[0].message);
+      return;
+    }
+    // res.send(result);
+    const user = new User(name, hashPassword, phone, email);
+
+    const promise = UserService.addUser(user);
+    promise
+      .then((data) => {
+        res.status(201).json({
+          message: 'Registration Successfully',
+        });
+      })
+      .catch((err) => {
+        res.status(500).json(err.message);
+      });
+  },
+  login(req, res) {
+    let email = req.body.email;
+    let password = req.body.password;
+    const promise = UserService.login(email);
+    promise
+      .then((data) => {
+        let pass = bcrypt.compare(password, data.password);
+        if (email === data.email && pass === true) {
+          const {password, ...others} = data._doc;
+          const accessToken = token.createToken({
+            id: data._id,
+            isAdmin: data.isAdmin,
+          });
+          res.status(200).json({...others, accessToken});
+        } else {
+          res.status(500).json({
+            message: 'Email And Password Not Match.....',
+          });
+        }
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+  //User Update By Verify Token
+  updateUser(req, res) {
+    let id = req.params.id;
+    let userData = req.body;
+    const promise = UserService.updateUser(id, userData);
+    promise
+      .then((data) => {
+        console.log(data);
+        res.status(201).json({
+          message: 'Update Successfully',
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  },
+  // Delete User
+  deleteUser(req, res) {
+    let id = req.params.id;
+    const promise = UserService.delete(id);
+    promise
+      .then((data) => {
+        console.log(data);
+        res.status(200).json({
+          message: 'Delete Successfully',
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  },
+
+  // fetch All Users
+  fetchAllUser(req, res) {
+    const query = req.query.new;
+    console.log(query);
+    const promise = UserService.getAllUsers(query);
+    promise
+      .then((data) => {
+        res.send(data);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  },
+};
+
+module.exports = userController;
--- db/connect.js
+++ db/connect.js
@@ -6,18 +6,20 @@
  * @summary Connection File
  */
 
- const mongoose = require('mongoose');
- const dbOptions = {
-     useNewUrlParser : true,
- };
- mongoose.connect("mongodb+srv://malav:malav12345@cluster0.dsd4p.mongodb.net/task?retryWrites=true&w=majority",dbOptions,(err)=>{
-     if(err){
-         console.log('DB Error',err.message)
-     }
-     else{
-         console.log('Connected to DB')
-     }
- });
- 
- 
- module.exports = mongoose;
+const mongoose = require('mongoose');
+const dbOptions = {
+  useNewUrlParser: true,
+};
+mongoose.connect(
+  'mongodb+srv://malav:malav12345@cluster0.dsd4p.mongodb.net/task?retryWrites=true&w=majority',
+  dbOptions,
+  (err) => {
+    if (err) {
+      console.log('DB Error', err.message);
+    } else {
+      console.log('Connected to DB');
+    }
+  }
+);
+
+module.exports = mongoose;
